58074c55e32a7178300726dd55d33562d0c7f7b8
JibinPrince <48076263+JibinPrince@users.noreply.github.com>
2019-02-27 20:48:09 -0600

    Update Main.java

--- GuessTheMovieGame/src/Main.java
+++ GuessTheMovieGame/src/Main.java
@@ -12,7 +12,7 @@ public class Main {
         String movieTitle = "";
 
         // Read given movie list file
-        File movieFile = new File("movies.txt");        // Access file containing list of movies
+        File movieFile = new File("movies.txt");                 // Access file containing list of movies
         Scanner movieScan = new Scanner(movieFile);              // Reads the file
         while(movieScan.hasNextLine()){
             String line = movieScan.nextLine();
@@ -23,8 +23,8 @@ public class Main {
         randomMovieNum = (int)((Math.random() * movieCount) + 1);
 
         // Get (randomMovieNum)th movie title       (Ex: if rand = 15, then get 15th movie title in text file)
-        File rMovieFile = new File("movies.txt");       // Access file containing list of movies
-        Scanner rMovieScan = new Scanner(rMovieFile);            // Reads the file
+        File rMovieFile = new File("movies.txt");               // Access file containing list of movies
+        Scanner rMovieScan = new Scanner(rMovieFile);           // Reads the file
         for(int i = 0; i < randomMovieNum; i++){
             movieTitle = rMovieScan.nextLine();
         }
@@ -34,8 +34,8 @@ public class Main {
         // Output commentary
         System.out.println("This is Guess the Movie Game!!!!");
 
-        Game attempt1 = new Game();                              // Declare new Game object
-        attempt1.dashedMovieTitle(movieTitle);                   // Start guessing game by calling Game class with random movie title
+        Game attempt1 = new Game();                             // Declare new Game object
+        attempt1.dashedMovieTitle(movieTitle);                  // Start guessing game by calling Game class with random movie title
 
     }
 }
